5956532b2da57258c1e5d4fbe919ae2b02112b8f
kimkangseok0613 <ks0006131@gmail.com>
2026-01-23 00:16:53 +0900

    (2026.01.23) Gomoku

--- Gomoku/Source.c
+++ Gomoku/Source.c
@@ -4,33 +4,39 @@
 
 #define DRAW_WHITE_STONE printf("●");
 #define DRAW_BLACK_STONE printf("○");
+#define MAP_X_1 2 + 2 * x
+#define MAP_Y_1 1 + y
+#define SIZE 15
+
+int board[20][20] = { 0 };
 
 int Position(int x, int y)
 {
 	COORD position = { x,y };
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position);
+	return 0;
 }
 
 int drawStone(int x, int y,int count)
-{	
-		Position(2 + 2 * x, 1 + y);
-		if (count % 2 == 1)
-		{
-			DRAW_WHITE_STONE
-				
-		}
-		else
-		{
-			DRAW_BLACK_STONE
-				
-		}
-		
+{
+	Position(MAP_X_1, MAP_Y_1);
+	if (count % 2 == 1)
+	{
+		DRAW_BLACK_STONE // 흑
+			board[x][y] = 1;			
+	}
+	else
+	{
+		DRAW_WHITE_STONE // 백
+			board[x][y] = 2;
+	}
+	return 0;
 }
 
 int drawMap()
 {
 	Position(4, 1);
-	for (int i = 0; i < 19; i++)
+	for (int i = 0; i < SIZE; i++)
 	{
 		if (i < 9)
 		{
@@ -43,31 +49,34 @@ int drawMap()
 		}
 	}
 	printf("\n");
-	for (int i = 0; i < 19; i++)
+	for (int i = 0; i < SIZE; i++)
 	{
 		printf("%d\n", i + 1);
 	}
 
-	for (int i = 0; i < 19; i++)
+	for (int i = 0; i < SIZE; i++)
 	{
 		Position(4, 2 + i);
 
-		for (int j = 0; j < 19; j++)
+		for (int j = 0; j < SIZE; j++)
 		{			
-			printf("┼ ");	
+			printf("┼ ");
 		}
 		printf("\n");
 	}
+	return 0;
 }
 
 int main()
 {
-	// printf("규칙");
-	// _getch();
-	// system("cls");
-	// 
+	 printf("[오목]\n\n");
+	 printf("1. 흑과 백이 번갈아가면서 돌을 둔다.\n\n");
+	 printf("2. 한쪽이 가로, 세로, 대각선으로 5개를 연결하면 게임 종료\n\n");
+	 printf("[Enter] 게임 시작");
+	 _getch();
+	 system("cls");
+	 
 
-	char key = 0;
 	int x=0;
 	int y=0;
 	int count = 1;
@@ -76,28 +85,37 @@ int main()
 
 	while (1)
 	{
-		int x, y;
-		Position(0, 22);
+		Position(0, 18);
 		if (count % 2 == 1)
-		{
-			printf("백의 차례입니다.\n");
-		}
-		else
 		{
 			printf("흑의 차례입니다.\n");
 		}
-		printf("x y의 값을 입력해주세요.[x y]\n");
-		scanf_s("%d %d", &x,&y);
-		printf("x : %d, y : %d", x, y);
-
-		if (x > 0 && y > 0 && x < 20 && y < 20)
+		else
 		{
+			printf("백의 차례입니다.\n");
+		}
+		printf("x y의 값을 입력해주세요.[x y]\n");
+		printf("\r                            \r");
+		scanf_s("%d %d", &x,&y);
+		printf("x : %d, y : %d           ", x, y);
+
+		if (x > 0 && y > 0 && x < SIZE+1 && y < SIZE+1)
+		{
+			if (board[x][y] != 0)
+			{
+				printf("\r이미 돌이 놓여있습니다! [Enter]        \r");
+				_getch();
+				printf("                                    ");
+				continue;
+			}
 			drawStone(x, y, count);
 			count += 1;
 		}
 		else
 		{
-			printf("다시 입력해주세요.");
+			printf("\r다시 입력해주세요.[Enter]                       \r");
+			_getch();
+			printf("                                        ");
 		}
 	}
 }
