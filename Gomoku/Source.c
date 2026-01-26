3f5a085ee17d992f6986fdb7207d700c2b4d0d69
kimkangseok0613 <ks0006131@gmail.com>
2026-01-26 21:46:20 +0900

    (2026.01.26) 오목

--- Gomoku/Source.c
+++ Gomoku/Source.c
@@ -5,6 +5,7 @@
 #define SIZE 15
 #define BLACK 1
 #define WHITE 2
+#define SPACE 0
 
 int board[SIZE][SIZE] = { 0 };
 
@@ -66,17 +67,69 @@ int drawStone(int x, int y, int count)
 	}
 }
 
+int printMenu()
+{
+	printf("[오목]\n\n");
+	printf("1. 흑과 백이 번갈아가면서 돌을 둔다.\n\n");
+	printf("2. 한쪽이 가로, 세로, 대각선으로 5개를 연결하면 게임 종료\n\n");
+	printf("[Enter] 게임 시작");
+	_getch();
+	system("cls");
+}
+
+int checkWin(int x, int y)
+{
+	int color = board[y][x];
+	int dx[4] = { 1, 0, 1, 1 };
+	int dy[4] = { 0, 1, 1, -1 };
+
+	for (int d = 0; d < 4; d++)
+	{
+		int count = 1;
+
+		for (int i = 1; i < 5; i++)
+		{
+			int nextX = x + dx[d] * i;
+			int nextY = y + dy[d] * i;
+			if (nextX < 0 || nextY < 0 || nextX >= SIZE || nextY >= SIZE) break;
+			if (board[nextY][nextX] != color) break;
+			count++;
+		}
+		for (int i = 1; i < 5; i++)
+		{
+			int nextX = x - dx[d] * i;
+			int nextY = y - dy[d] * i;
+			if (nextX < 0 || nextY < 0 || nextX >= SIZE || nextY >= SIZE) break;
+			if (board[nextY][nextX] != color) break;
+			count++;
+		}
+
+		if (count >= 5) return 1;
+	}
+	return 0;
+}
+
+int printResult(int count)
+{
+	if (count % 2 == 1)
+	{
+		Position(0, 20);
+		printf("\n흑 승리! 게임종료\n");
+	}
+	else
+	{
+		Position(0, 20);
+		printf("\n백 승리! 게임 종료\n");
+	}
+}
+
 int main()
 {
 	int x, y;
 	int count = 1;
+	int input = 0;
 
-	// printf("[오목]\n\n");
-	// printf("1. 흑과 백이 번갈아가면서 돌을 둔다.\n\n");
-	// printf("2. 한쪽이 가로, 세로, 대각선으로 5개를 연결하면 게임 종료\n\n");
-	// printf("[Enter] 게임 시작");
-	// _getch();
-	// system("cls");
+	// printMenu();
 
 	drawMap();
 
@@ -93,11 +146,18 @@ int main()
 		}
 		printf("x y 값을 입력해주세요.[x y]\n");
 		printf("\r                                 \r");
-
-		scanf_s("%d %d", &x,&y);
-
+		
+		if (scanf_s("%d %d", &x, &y) != 2)
+		{
+			break;
+		}
+		else
+		{
+			continue;
+		}
+		
 		x-=1; y-=1;
-
+		
 		if (x < 0 || y < 0 || x >= SIZE || y >= SIZE)
 		{
 			Position(0, 20);
@@ -113,7 +173,13 @@ int main()
 			continue;
 		}
 		
-		drawStone(x, y, count);
+		drawStone(x, y, count);	
+
+		if(checkWin(x, y))
+		{
+			printResult(count);
+			break;
+		}
 
 		count++;
 	}
